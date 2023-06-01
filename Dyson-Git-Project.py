85d6e296b1efbb2c2eb3e21c16cb567fad739808
Jayavel250387 <Jayavel.maniyan@dyson.com>
2023-06-01 13:45:41 +0800

    first commit

---
+++ Dyson-Git-Project.py
@@ -0,0 +1,30 @@
+"""
+This program is used to write a simple operations of math. 
+"""
+
+# Write here your operation functions:
+
+
+# Usage example:
+num1 = 5
+num2 = 3
+
+# Team Member 1
+result_add = add(num1, num2)
+print("Addition result:", result_add)
+
+# Team Member 2
+result_subtract = subtract(num1, num2)
+print("Subtraction result:", result_subtract)
+
+# Team Member 3
+result_multiply = multiply(num1, num2)
+print("Multiplication result:", result_multiply)
+
+# Team Member 4
+result_divide = divide(num1, num2)
+print("Division result:", result_divide)
+
+# Team Member 5
+result_exponentiate = exponentiate(num1, num2)
+print("Exponentiation result:", result_exponentiate)
